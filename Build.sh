acb8b1f66b7267ca5cd01b92ab9f86ab8bd2b5e0
Ekaterina Volkova <97599621+ekateluv@users.noreply.github.com>
2024-03-04 16:44:22 +0300

    Update Build.sh
    Co-authored-by: Nikolay Korotkiy <sikmir@disroot.org>

--- Build.sh
+++ Build.sh
@@ -5,8 +5,8 @@ DIST_DIR=dist
 VENV_DIR=.venv_docker
 
 linux() {
-	python3 -m venv $(VENV_DIR)
-	. $(VENV_DIR)/bin/activate
+	python3 -m venv $VENV_DIR
+	. $VENV_DIR/bin/activate
 	pip install -r requirements.txt
 	pyinstaller --distpath $DIST_DIR/linux ./wb-modbus-device-editor.spec
 }
