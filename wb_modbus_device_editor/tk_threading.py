8abe90b3b6910e18c646f5843a2653bf55b01e5d
Ekaterina Volkova <ekaterina.volkova@wirenboard.ru>
2024-04-01 10:46:25 +0300

    fix pylint

--- wb_modbus_device_editor/tk_threading.py
+++ wb_modbus_device_editor/tk_threading.py
@@ -1,10 +1,10 @@
 import queue
 import threading
-import traceback
 
 
+# disable=dangerous-default-value
 def run_in_thread(window, computation, args=(), kwargs={}, callback=None, errback=None, polling=500):
-    future_result= queue.Queue()
+    future_result = queue.Queue()
 
     worker = threading.Thread(target=_compute_result, args=(computation, args, kwargs, future_result))
     worker.daemon = True
@@ -13,19 +13,21 @@ def run_in_thread(window, computation, args=(), kwargs={}, callback=None, errbac
     if callback is not None or errback is not None:
         _after_completion(window, future_result, callback, errback, polling)
 
+
 def _compute_result(computation, args, kwargs, future_result):
-    try: 
+    try:
         _result = computation(*args, **kwargs)
-    except BaseException as e:
+    except BaseException as e: #disable=broad-except
         _result = e
 
     future_result.put(_result)
+
 
 def _after_completion(window, future_result, callback, errback, polling):
     def check():
         try:
             result = future_result.get(block=False)
-        except Exception:
+        except Exception: #disable=broad-except
             window.after(polling, check)
         else:
             if isinstance(result, Exception):
@@ -34,5 +36,5 @@ def _after_completion(window, future_result, callback, errback, polling):
             else:
                 if callback is not None:
                     callback(result)
-                
+
     window.after(0, check)
