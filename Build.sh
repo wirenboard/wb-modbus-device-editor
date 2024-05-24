73c349f38fa637abfb6e2d2aaf0aeda0da468de0
Ekaterina Volkova <97599621+ekateluv@users.noreply.github.com>
2024-05-24 11:06:11 +0300

    Add appimage build (#18)

--- Build.sh
+++ Build.sh
@@ -5,16 +5,14 @@ DIST_DIR=dist
 VENV_DIR=.venv_docker
 
 linux() {
-	python3 -m venv $VENV_DIR
-	. $VENV_DIR/bin/activate
 	pip install pyinstaller
 	pip install -r requirements.txt
-	pyinstaller --distpath $DIST_DIR/linux ./wb-modbus-device-editor.spec
+	pyinstaller --distpath $DIST_DIR/linux ./linux.spec
 }
 
 windows() {
 	wine pip install -r requirements.txt
-	wine pyinstaller --distpath $DIST_DIR/windows ./wb-modbus-device-editor.spec
+	wine pyinstaller --distpath $DIST_DIR/windows ./windows.spec
 }
 
 clean() {
